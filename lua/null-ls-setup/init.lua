46420e42e9bbce416dda6b4104ce1f681ab91d59
zerochae <zerochae@kakao.com>
2023-09-23 21:14:42 +0900

    🐛 Fix: 폴더이동

--- lua/null-ls-setup/init.lua
+++ lua/null-ls-setup/init.lua
@@ -1,4 +1,3 @@
-local M = {}
 local present, null_ls = pcall(require, "null-ls")
 
 if not present then
@@ -7,11 +6,6 @@ end
 
 local sources = require "sources"
 
-M.init = function ()
-  null_ls.setup {
-    sources = sources,
-  }
-end
-
-
-return M
+null_ls.setup {
+  sources = sources,
+}
